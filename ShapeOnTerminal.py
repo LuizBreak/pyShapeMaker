e145baaa71d950a0a2d3423c7077b352c2664b7a
Luiz Break <luiz.break@gmail.com>
2021-11-06 22:43:53 -0400

    Command line args implemented

--- ShapeOnTerminal.py
+++ ShapeOnTerminal.py
@@ -1,8 +1,33 @@
+import sys
 
-# Run application: python ShapeOnTerminal.py terminal false "./Data/json/shapesRequest.v4.json"
+# Run application: python ShapeOnTerminal.py terminal "./Data/json/shapesRequest.v4.json"
 
 def main():
+    # Article Reference: https://www.knowledgehut.com/blog/programming/sys-argv-python-examples
+    print ("No. of arguments passed is {}".format(len(sys.argv)))
+
+    for idx, arg in enumerate(sys.argv):
+        if idx == 2:
+            outpuType = arg
+            print("Argument #{} outpuType -> {}".format(idx, arg))
+        elif idx == 3:
+            filePath = arg
+            print("Argument #{} filePath -> {}".format(idx, arg))
+
+    if len(filePath)>0:
+        processJsonRequests(filePath, outputType)
+        sys.exit(0)
+    else:
+        print("Error: Json path not provided!")
+        sys.exit(1)
+
+def processJsonRequests(filePath, outputType):
     return ""
 
-def processJsonRequests(filePath, flashOnScreen):
-    return ""
+lado = ""
+centro = ""
+outputType = "web"
+flashOnScreen = ""
+filePath = ""
+
+main()
