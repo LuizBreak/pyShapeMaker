c47934f37c3b92edf74241b5693a8ce5a19b7126
Luiz Break <luiz.break@gmail.com>
2021-11-06 20:24:02 -0400

    creating functions mockup to start coding

---
+++ ShapeOnTerminal.py
@@ -0,0 +1,8 @@
+
+# Run application: python ShapeOnTerminal.py terminal false "./Data/json/shapesRequest.v4.json"
+
+def main():
+    return ""
+
+def processJsonRequests(filePath, flashOnScreen):
+    return ""
---
+++ drawShape.py
@@ -0,0 +1,38 @@
+import random
+import sys
+import os
+
+
+def ShapeController(c1, c2, c3, outputType, shapeType, flash, Ratio):
+    return ""
+
+def MakeCross(c1, c2, outputType, flash, ratio):
+    return ""
+
+def MakeDiamond(c1, c2, outputType, flash, ratio):
+    return ""
+
+
+def MakeSquare(c1, c2, outputType, flash, ratio):
+    return ""
+
+def Makerhombus(c1, c2, outputType, flash, ratio):
+    return ""
+
+def MakeEnvelope(c1, c2, outputType, flash, ratio):
+    return ""
+
+def GetLineFeed (outputType, flash):
+    return ""
+
+def Izquierda(Tamano, CaracterDeseado, flash):
+    return ""
+
+def Centro(Tamano, CaracterDeseado, flash):
+    return ""
+
+def Derecha(Tamano, CaracterDeseado, flash):
+    return ""
+
+def PonerLetras(Tamano, LetraDeseada, flash=false):
+    return ""
