48e0b38613c647cfbfe981f5bc1d3ebab9a32b91
Luiz Break <luiz.break@gmail.com>
2021-11-07 22:41:08 -0400

    calculating elapsed time

--- ShapeOnTerminal.py
+++ ShapeOnTerminal.py
@@ -1,25 +1,35 @@
 import sys
+import time
 
 # Run application: python ShapeOnTerminal.py terminal "./Data/json/shapesRequest.v4.json"
+# Aritcle Reference: Great python snippets for day to day stuff: https://therenegadecoder.com/code/python-code-snippets-for-everyday-problems/
 
 def main():
+
+    start_time = time.time()
+
     # Article Reference: https://www.knowledgehut.com/blog/programming/sys-argv-python-examples
     print ("No. of arguments passed is {}".format(len(sys.argv)))
 
     for idx, arg in enumerate(sys.argv):
-        if idx == 2:
+        if idx == 1:
             outpuType = arg
             print("Argument #{} outpuType -> {}".format(idx, arg))
-        elif idx == 3:
+        elif idx == 2:
             filePath = arg
             print("Argument #{} filePath -> {}".format(idx, arg))
 
     if len(filePath)>0:
         processJsonRequests(filePath, outputType)
-        sys.exit(0)
+
     else:
         print("Error: Json path not provided!")
         sys.exit(1)
+
+    end_time = time.time()
+    time_taken = end_time - start_time
+    print("Time: #{}".format(time_taken))
+    sys.exit(0)
 
 def processJsonRequests(filePath, outputType):
     return ""
